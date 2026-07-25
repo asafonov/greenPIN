346464287123461baa73c5a778f5177eecac7509
asafonov <me@asafonov.org>
2026-07-25 13:40:43 +0000

    fix url parser

--- models/totp.js
+++ models/totp.js
@@ -3,13 +3,21 @@ class TOTP {
   }
 
   parseUrl (urlString) {
-    const url = new URL(urlString)
-    const pathname = url.pathname.substr(1).split(':')
+    const url = urlString.replace(/[A-z]*\:\/\/[A-z]+\//g, '').split('?')
+    const pathname = url[0].split(':')
+    const params = {}
+    const parts = url.length > 1 ? url[1].split('&') : []
+
+    for (let i = 0; i < parts.length; ++i) {
+      const nv = parts[i].split('=')
+      params[nv[0]] = nv[1] || ''
+    }
+
     return {
-      secret: url.searchParams.get('secret'),
-      issuer: url.searchParams.get('issuer'),
+      secret: params.secret || '',
+      issuer: params.issuer || '',
       provider: pathname[0],
-      username: pathname.length > 1 ? pathname[1] : null
+      username: pathname.length > 1 ? pathname[1] : ''
     }
   }
 
