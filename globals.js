57785a6f361a058e8af2e34738971d4150fa5211
asafonov <me@asafonov.org>
2026-08-04 07:56:32 +0000

    updater initial implementation

--- globals.js
+++ globals.js
@@ -2,6 +2,7 @@ window.asafonov = {}
 window.asafonov.messageBus = new MessageBus()
 window.asafonov.totp = new TOTP()
 window.asafonov.clipboard = new Clipboard()
+window.asafonov.version = '0.7'
 window.asafonov.events = {
   ITEM_ADDED: 'ITEM_ADDED',
   ITEM_DELETED: 'ITEM_DELETED',
